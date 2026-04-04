4079158add34809e2ed2bd57d1df303934a2b8f3
Kai Rawal <80235810+KaiRawal@users.noreply.github.com>
2026-04-04 18:03:46 +0100

    Create ExpsForR3.sh

---
+++ ExpsForR3.sh
@@ -0,0 +1,22 @@
+
+
+
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/qwen3-0.6b 5
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/qwen3-0.6b 8
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/qwen3-4b 5
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/qwen3-4b 8
+
+
+
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-1b-Instruct 5
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-1b-Instruct 8
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama3.2-3b-Instruct 5
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama3.2-3b-Instruct 8
+
+
+
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-1b-it 5
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-1b-it 8
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-4b-it 5
+bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-4b-it 8
+
