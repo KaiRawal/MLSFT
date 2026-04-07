c8389f58f60b08f50ddfd80cfdee970d665d8b58
kai <kai@virgil.oii.ox.ac.uk>
2026-04-07 08:07:43 +0100

    gemma 4B E5 mega batch all E5 results (32-1)

--- ExpsForR3.sh
+++ ExpsForR3.sh
@@ -9,14 +9,14 @@
 
 #bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-1b-Instruct 5
 #bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-1b-Instruct 8
-bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-3b-Instruct 5
-bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-3b-Instruct 8
+#bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-3b-Instruct 5
+#bash scripts/run_param_multilang_direct_pipeline.sh unsloth/llama-3.2-3b-Instruct 8
 
 
 
 #bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-1b-it 5
 #bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-1b-it 8
 bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-4b-it 5
-bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-4b-it 8
+#bash scripts/run_param_multilang_direct_pipeline.sh unsloth/gemma-3-4b-it 8
 
 
